c1f82dc3d030deafb71037dc65f4b80e6fdb1e24
Mubasher Chaudhary <follow.with.mubasher@gmail.com>
2026-08-24 03:55:48 -0700

    Update downloader.ps1

--- downloader.ps1
+++ downloader.ps1
@@ -1,5 +1,5 @@
 # Downloads and runs a script from a URL
-$url = "https://github.com/CodewithMubasher/Scripts/blob/main/script.ps1"
+$url = "https://raw.githubusercontent.com/CodewithMubasher/Scripts/main/script.ps1"
 $tempFile = Join-Path $env:TEMP "script.ps1"
 Invoke-WebRequest -Uri $url -OutFile $tempFile
 & $tempFile
